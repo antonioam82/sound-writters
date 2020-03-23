8eb3141a589c2453c7e2da5335f83aacfe702005
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-23 17:26:53 +0100

    Update audio_loop_writter.py

--- audio_loop_writter.py
+++ audio_loop_writter.py
@@ -2,18 +2,6 @@ from VALID import OKI, ns, OK
 from pydub import AudioSegment
 from pydub.playback import play
 from pydub.generators import Sine
-import glob
-
-def guardar(sound):
-    grabs = glob.glob('*.mp3')
-    count = 0
-    for i in grabs:
-        if "audd" in i:
-            count+=1
-    if count>0:
-        sound.export("audd"+str(count)+".mp3",format="mp3")
-    else:
-        sound.export("audd.mp3",format="mp3")
 
 while True:
     print("-----------------LOOP SOUND WRITTER-----------------\n")
@@ -22,21 +10,25 @@ while True:
     f = OK(input("Frequency adding: "))
     fi = OK(input("Fade in: "))#50
     fo = OK(input("Fade out: "))#100
-
-    for n in range(t):
-        gen = Sine(f * n)
-        sine = gen.to_audio_segment(duration=t)
-        sine = sine.fade_in(fi).fade_out(fo)
-        result += sine
-    print("REPRODUCIENDO RESULTADO...")
-    play(result)
+    try:
+        for n in range(t):
+            gen = Sine(f * n)
+            sine = gen.to_audio_segment(duration=t)
+            sine = sine.fade_in(fi).fade_out(fo)
+            result += sine
+        print("REPRODUCIENDO RESULTADO...")
+        play(result)
     
-    guard = ns(input("¿Guardar sonido?(n/s): "))
-    if guard == "s":
-        guardar(result)
+        guard = ns(input("¿Guardar sonido?(n/s): "))
+        if guard == "s":
+            name = "audd"+"("+str(t)+","+str(f)+","+str(fi)+","+str(fo)+")"+".mp3"
+            result.export(name,format="mp3")
+    except:
+        print("SE HA PRODUCIDO UN ERROR")
         
     conti = ns(input("¿Continuar?(n/s): "))
     if conti == "n":
         break
+    
     
     
