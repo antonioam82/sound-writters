a8394ed408dde74881f8a4103b4394ef5ebcba07
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-04-01 22:17:00 +0200

    Update tonethizerGUI.py

--- tonethizerGUI.py
+++ tonethizerGUI.py
@@ -6,7 +6,20 @@ from tkinter import *
 from tkinter import messagebox, filedialog
 
 def play_sound():
-    print("HELLO")
+    result = AudioSegment.silent(duration=0)
+    t=int(tmes.get())
+    f=float(freq.get())
+    fi=float(fadein.get())
+    fo=float(fadeo.get())
+    try:
+        for n in range(t):
+            gen = Sine(f*n)
+            sine = gen.to_audio_segment(duration=t)
+            sine = sine.fade_in(fi).fade_out(fo)
+            result+=sine
+        play(result)
+    except:
+        print("Algo salio mal")
 
 
 root = tkinter.Tk()
@@ -14,14 +27,20 @@ root.title("ToneThizer")
 root.configure(background="gray45")
 root.geometry("300x300")
 times = IntVar()
-#frequency = IntVar()
-#fadein = IntVar()
+frequency = DoubleVar()
+fadein = DoubleVar()
+fadeon = DoubleVar()
 
-Entry(root,textvariable=times,bg="khaki",width=20).place(x=120,y=40)
-Entry(root,textvariable=times,bg="khaki",width=20).place(x=120,y=80)
-Entry(root,textvariable=times,bg="khaki",width=20).place(x=120,y=120)
-Entry(root,textvariable=times,bg="khaki",width=20).place(x=120,y=160)
+tmes=Entry(root,textvariable=times,bg="khaki",width=20)
+tmes.place(x=120,y=40)
+freq=Entry(root,textvariable=frequency,bg="khaki",width=20)
+freq.place(x=120,y=80)
+fadei=Entry(root,textvariable=fadein,bg="khaki",width=20)
+fadei.place(x=120,y=120)
+fadeo=Entry(root,textvariable=fadeon,bg="khaki",width=20)
+fadeo.place(x=120,y=160)
 
 Button(root,text="PLAY",command=play_sound).place(x=60,y=190)
 
 root.mainloop()
+
