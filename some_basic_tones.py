014bc0c8a0e99b83a40f9e3571d859b1bf04d345
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-03 11:06:41 +0100

    Update some_basic_tones.py

--- some_basic_tones.py
+++ some_basic_tones.py
@@ -1,15 +1,25 @@
 from tkinter import *
 from pydub import AudioSegment
-from pydub.generators import Sine, Square, Triangle
+from pydub.generators import Sine, Square, Triangle, Sawtooth
 from pydub.playback import play
 
 class app():
     def __init__(self):
         self.window = Tk()
-        self.window.geometry("200x200")
+        self.window.geometry("400x400")
         self.window.title("SBT")
+
+        self.drlabel = Label(self.window,text="DURATION:")
+        self.drlabel.place(x=40,y=20)
+        self.drentry = Entry(self.window,width=20)
+        self.drentry.place(x=110,y=20)
+        self.frlabel = Label(self.window,text="FREQUENCY:")
+        self.frlabel.place(x=33,y=70)
+        self.frentry = Entry(self.window,width=20)
+        self.frentry.place(x=110,y=70)
 
         self.window.mainloop()
 
 if __name__=="__main__":
     app()
+
