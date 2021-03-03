ebd457aff071438e9c36e0e3388c52e4ec592a14
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-03 11:53:19 +0100

    Update some_basic_tones.py

--- some_basic_tones.py
+++ some_basic_tones.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from pydub import AudioSegment
 from pydub.generators import Sine, Square, Triangle, Sawtooth
 from pydub.playback import play
@@ -8,6 +9,7 @@ class app():
         self.window = Tk()
         self.window.geometry("400x400")
         self.window.title("SBT")
+        self.WaveForms = ["Sine","Square","Triangle","Sawtooth"]
 
         self.drlabel = Label(self.window,text="DURATION:")
         self.drlabel.place(x=40,y=20)
@@ -17,9 +19,15 @@ class app():
         self.frlabel.place(x=33,y=70)
         self.frentry = Entry(self.window,width=20)
         self.frentry.place(x=110,y=70)
+        self.wfentry = ttk.Combobox(self.window,width=17)
+        self.wfentry.place(x=110,y=120)
+        self.wflabel = Label(self.window,text="WAVEFORM:")
+        self.wflabel.place(x=33,y=120)
+        self.wfentry["values"] = self.WaveForms
 
         self.window.mainloop()
 
 if __name__=="__main__":
     app()
+
 
